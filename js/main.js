3bafb3d72adca930a99105d70e18bc6c11186776
Th4Dude <gallottimaurizio@yahoo.it>
2023-02-23 19:21:29 +0200

    bonus 1 done

--- js/main.js
+++ js/main.js
@@ -4,26 +4,25 @@
 const ul = document.querySelector('ul.list')
 
 for (let i = 1; i <= 100; i++) {
-    
-    if ( i % 3 === 0 && i % 5 === 0) {
+
+    const elementoLista = document.createElement('li')
+
+    if (i % 3 === 0 && i % 5 === 0) {
         console.log("FizzBuzz");
-        const elementoLista = document.createElement('li')
+
         elementoLista.append('fizzbuzz')
         ul.append(elementoLista)
-    } else if ( i % 3 === 0) {
+    } else if (i % 3 === 0) {
         console.log("Fizz");
-        const elementoLista = document.createElement('li')
         elementoLista.append('fizz')
         ul.append(elementoLista)
-    } else if ( i % 5 === 0) {
+    } else if (i % 5 === 0) {
         console.log("Buzz");
-        const elementoLista = document.createElement('li')
         elementoLista.append('buzz')
         ul.append(elementoLista)
     } else {
         console.log(i);
-        const elementoLista = document.createElement('li')
-        elementoLista.append('i')
+        elementoLista.innerHTML = (i);
         ul.append(elementoLista)
     }
 }
