66b88a226c17a439587bf404d563fe730810fc64
Th4Dude <gallottimaurizio@yahoo.it>
2023-02-24 11:57:32 +0200

    fix js

--- js/main.js
+++ js/main.js
@@ -6,22 +6,19 @@ const ul = document.querySelector('ul.list')
 for (let i = 1; i <= 100; i++) {
 
     const elementoLista = document.createElement('li')
+    ul.append(elementoLista)
 
     if (i % 3 === 0 && i % 5 === 0) {
         console.log("FizzBuzz");
         elementoLista.append('fizzbuzz')
-        ul.append(elementoLista)
     } else if (i % 3 === 0) {
         console.log("Fizz");
         elementoLista.append('fizz')
-        ul.append(elementoLista)
     } else if (i % 5 === 0) {
         console.log("Buzz");
         elementoLista.append('buzz')
-        ul.append(elementoLista)
     } else {
         console.log(i);
         elementoLista.innerHTML = (i);
-        ul.append(elementoLista)
     }
 }
